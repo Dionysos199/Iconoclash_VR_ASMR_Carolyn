c56cdd21cf8af96985a38a36cb3032d5f4923628
Dionysos199 <imadelkhechen@gmail.com>
2022-06-21 22:22:20 +0200

    checkPoints move towards
    -Vector3.Movetowards
    -TriggerEnter did work with the XR Origin
    -Just check the distance if very small next check point is the next on the list

---
+++ ASMR_VR/Assets/moveCheckPoints.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCheckPoints : MonoBehaviour
+{
+    private GameObject nextCheckpoint;
+    public List<GameObject> checkpoints;
+    Transform _transform;
+    int index = 1;
+    Vector3 direction;
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+        _transform.position = checkpoints[0].transform.position;
+        nextCheckpoint = checkpoints[1];
+        _transform.rotation = Quaternion.FromToRotation(Vector3.forward, direction);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("you reached a checkpoint");
+        Debug.Log(other);
+        if (other.gameObject == nextCheckpoint)
+        {
+            Debug.Log("you reached a checkpoint");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        // direction = nextCheckpoint.transform.position - _transform.position;
+
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        // transform.rotation = Quaternion.LookRotation(direction);
+
+        // Draw a ray pointing at our target in
+        //  Debug.DrawRay(transform.position, direction, Color.red);
+
+        //  _transform.localPosition += Vector3.forward * Time.deltaTime;
+
+
+        var step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, nextCheckpoint.transform.position, step);
+        Vector3 deltaPosition = transform.position - nextCheckpoint.transform.position;
+        if (deltaPosition.magnitude < 0.1)
+        {
+            if(index < checkpoints.Count) index++;
+            nextCheckpoint = checkpoints[index];
+        }
+    }
+}
